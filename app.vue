f697c41dd94d483748e1366a3ee567c4f4607ef9
Tyrone Jose <132106425+tyronejosee@users.noreply.github.com>
2024-06-27 15:08:37 -0400

    feat: add basic tests

--- app.vue
+++ app.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <template>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </template>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
